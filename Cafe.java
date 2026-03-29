e32354ddff3c27c64bd806131831d048db829bbc
JasLee07 <jrlee@smith.edu>
2026-03-29 13:08:44 -0400

    Cafe done (?)

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,43 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building implements CafeRequirements {
+    private int nCoffeeOunces;
+    private int nSugarPackets;
+    private int nCreams;
+    private int nCups;
 
-    public Cafe() {
-        System.out.println("You have built a cafe: ☕");
+    public Cafe(String name, String address, int nFloors, int nCoffeeOunces, 
+        int nSugarPackets, int nCreams, int nCups) {
+        super(name, address, nFloors);
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
+        System.out.println("You have built a cafe.");
     }
-    
+
+    public void sellCoffee(int size, int nSugarPackets, int nCreams){
+        System.out.println("One latte coming up!");
+        this.nCoffeeOunces -= size;
+        this.nSugarPackets -= nSugarPackets;
+        this.nCreams -= nCreams;
+        this.nCups -= 1;
+        System.out.println("Here is your latte!");
+    }
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups){
+        this.nCoffeeOunces += nCoffeeOunces;
+        this.nSugarPackets += nSugarPackets;
+        this.nCreams += nCreams;
+        this.nCups += nCups;
+        System.out.println("Items stocked");
+    }
+
     public static void main(String[] args) {
-        new Cafe();
-    }
-    
+        Cafe DashCafe = new Cafe("Cafe le Dash", "123 B Street", 2, 
+        300, 300, 200, 70);
+        System.out.println("Welcome! What can I get for you?");
+        DashCafe.sellCoffee(16, 7, 4);
+        System.out.println("Enjoy your day!");
+        DashCafe.restock(300, 300, 200, 70);
+    }   
 }

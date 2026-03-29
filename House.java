9062851f28066d25dfdd97872dda36b88d986854
JasLee07 <jrlee@smith.edu>
2026-03-28 20:06:13 -0400

    House Class Finished (?)

--- House.java
+++ House.java
@@ -1,12 +1,58 @@
 /* This is a stub for the House class */
-public class House {
 
-  public House() {
-    System.out.println("You have built a house: 🏠");
+import java.util.ArrayList;
+
+
+public class House extends Building implements HouseRequirements {
+
+  /*
+  The <Student> tells Java 
+  what kind of data we plan to store IN the ArrayList
+   */ 
+  private ArrayList<Student> residents;
+  public boolean hasDiningRoom;
+  public int nResidents;
+  
+  public House(String name, String address, int nFloors, int nResidents, boolean hasDiningRoom) {
+  super(name, address, nFloors);
+  System.out.println("You have built a house: 🏠");
+  this.residents = new  ArrayList<Student>();
+  this.hasDiningRoom = hasDiningRoom;;
+  
   }
 
+  public int nResidents(){
+    return nResidents;
+  }
+
+  public boolean hasDiningRoom() {
+    return this.hasDiningRoom;
+    
+  }
+
+  public void moveIn(Student s){
+    if(!residents.contains(s)){
+    residents.add(s);
+    }
+  }
+
+  public Student moveOut(Student s){
+    residents.remove(s);
+    return s;
+  }
+
+  public boolean isResident(Student s){
+    return this.isResident(s);
+  }
   public static void main(String[] args) {
-    new House();
-  }
+    House jordanHouse = new House("Jordan House", "1 Paradise Rd", 4, 95, false);
 
+    System.out.println(jordanHouse);
+  }
 }
+
+
+
+
+
+
